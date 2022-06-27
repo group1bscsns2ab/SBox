b6c1587b0a06914863e26fe35b71f224454728ff
Daniel Shan Balico <danielshan.balico@gmail.com>
2022-06-27 11:56:22 +0800

    added PC get

--- server/routes/device.route.js
+++ server/routes/device.route.js
@@ -65,6 +65,18 @@ router.route('/PC/:ipaddress/:macaddress').get((req, res) => {
 		})	
 });
 
+router.route('/PC/get').get((req, res) => {
+	const ipaddress = req.params.ipaddress;
+	const macaddress = req.params.macaddress;
+
+	Data.findById(process.env.DOCID)
+		.then(data => {
+			data.PC[0] = ipaddress;
+			data.PC[1] = macaddress;
+
+			res.send({ PC: data.PC, success: true});
+		})	
+});
 
 router.route('/reset').get((req, res) => {
 	Data.findById(process.env.DOCID)
